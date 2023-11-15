b2ec7655760a8d3ff03fc8535f8f6ee0cc2a9c22
Ayaz Maroof <maroof.ayaz@gmail.com>
2023-11-15 11:00:31 -0600

    Added tailwind + sass.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@
 import { RouterLink, RouterView } from 'vue-router';
 import { onMounted, ref } from 'vue';
 import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
-import { useRouter} from 'vue-router';
+import { useRouter } from 'vue-router';
 
 const router = useRouter();
 const isLoggedIn = ref(false);
@@ -17,86 +17,25 @@ onMounted(() => {
 
 const handleSignOut = () => {
   signOut(auth).then(() => {
-    router.push('/')
+    router.push('/');
   });
 };
 </script>
 
 <template>
-    <div class="wrapper">
-      <p v-if="isLoggedIn">You are logged in!</p>
-      <p v-if="isLoggedIn">{{ auth.currentUser.email }}</p>
+  <div class="wrapper">
+    <p v-if="isLoggedIn">You are logged in!</p>
+    <p v-if="isLoggedIn">{{ auth.currentUser.email }}</p>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink v-if="!isLoggedIn" to="/register">Register</RouterLink>
-        <RouterLink v-if="!isLoggedIn" to="/login">Login</RouterLink>
-        <button @click="handleSignOut" v-if="isLoggedIn">Sign Out</button>
-      </nav>
-    </div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink v-if="!isLoggedIn" to="/register">Register</RouterLink>
+      <RouterLink v-if="!isLoggedIn" to="/login">Login</RouterLink>
+      <button @click="handleSignOut" v-if="isLoggedIn">Sign Out</button>
+    </nav>
+  </div>
 
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
